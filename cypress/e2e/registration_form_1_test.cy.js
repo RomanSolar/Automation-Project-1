08286650ccae4e5dd4f33139ced569c62bf6f95f
RomanSolar <rsobo77@gmail.com>
2024-07-21 04:43:55 +0300

    My commets about the commit

--- cypress/e2e/registration_form_1_test.cy.js
+++ cypress/e2e/registration_form_1_test.cy.js
@@ -16,12 +16,14 @@ Assignment 2:
  5. Add comment to the first test containing today’s date
  */
 
-describe('This is first test suite', () => {
+ //Date: July 8, 2024
+describe('This is first test suite Roman Sobolenko Solar', () => {
     it('User can submit data only when valid mandatory values are added', () => {
+        
+        cy.get('[data-testid="phoneNumberTestId"]').type('555666777')
+        cy.get('input[name="password"]').type('NewPassword123RS')
+        cy.get('[name="confirm"]').type('NewPassword123RS')
         cy.get('#username').type('Something')
-        cy.get('[data-testid="phoneNumberTestId"]').type('5656565656')
-        cy.get('input[name="password"]').type('Password123')
-        cy.get('[name="confirm"]').type('Password123')
 
         //in order to activate submit button, user has to click somewhere outside the input field
         cy.get('h2').contains('Password').click()
@@ -97,12 +99,14 @@ describe('This is first test suite', () => {
     */
 
     it('User cannot submit data when phone number is absent', () => {
+        cy.get('[data-testid="phoneNumberTestId"]').type('555666777')
         // Add test, similar to previous one with phone number field not filled in
         // All other fields should be entered correctly
         // Assert that submit button is not enabled and that successful message is not visible
     })
 
     it('User cannot submit data when password and/or confirmation password is absent', () => {
+        cy.get('input[name="password"]').type('NewPassword123RS')
         // Add test, similar to previous one with password field not filled in
         // All other fields should be entered correctly
         // Assert that submit button is not enabled and that successful message is not visible
